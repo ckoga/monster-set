292cf15a7db048622297fee05bb3b7f4c709122a
Colin Turing <colin.koga@gmail.com>
2020-01-09 16:30:49 -0700

    Add EquipContainer to display armor and weapon cards, styled the component

--- src/containers/EquipContainer.js
+++ src/containers/EquipContainer.js
@@ -2,25 +2,32 @@ import React from 'react';
 import ArmorCard from '../Components/ArmorCard';
 import WeaponCard from '../Components/WeaponCard';
 import { connect } from 'react-redux';
+import './EquipContainer.scss'
 
 const EquipContainer = ({ armors, weapons }) => {
   let cards = armors.map(piece => {
-    return <ArmorCard
-      key={piece.name}
+    return (<ArmorCard
+      key={piece.id}
+      img={piece.assets.imageMale && piece.assets.imageFemale}
       name={piece.name}
       type={piece.type}
       rank={piece.rank}
       rarity={piece.rarity}
-
-    />
+      defense={piece.defense}
+    />)
   })
 
   
   return (
-    <main className='equip-container'>
-      {cards}
-      
-    </main>
+    <div className='whole-container'>
+      <div className='button-div'>
+        <button className='weapon-btn' type='button'>Weapons</button>
+        <button className='armor-btn' type='button'>Armor</button>
+      </div>
+      <main className='equip-container'>
+        {cards}
+      </main>
+    </div>
   )
 }
 
