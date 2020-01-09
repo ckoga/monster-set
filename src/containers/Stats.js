0ca7490e4cbed15581285a446ba2f0171b11f245
Colin Turing <colin.koga@gmail.com>
2020-01-09 16:31:27 -0700

    Add Stats component and and styling

---
+++ src/containers/Stats.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Stats.scss'
+
+
+const Stats = () => {
+  return (
+    <aside className='stats-container'>
+      <h3>Equipment Stats</h3>
+      <ul className='stats-list'>
+        <li>name: name</li>
+        <li></li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Stats;
