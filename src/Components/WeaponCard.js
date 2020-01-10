7f04c2fda0197afd1dd489d509af21d372d3e0ab
Colin Turing <colin.koga@gmail.com>
2020-01-09 18:02:38 -0700

    Add WeaponCard component and add styles

---
+++ src/Components/WeaponCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './WeaponCard.scss';
+
+const WeaponCard = ({ img, name, type, rarity, attack }) => {
+  return (
+    <div className='weapon-card'>
+      <img className='equip-img' src={img}></img>
+      <h4 className='weapon-name'>{name}</h4>
+      <ul>
+        <li>type: {type}</li>
+        <li>rarity: {rarity}</li>
+        <li>attack: {attack}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default WeaponCard;
