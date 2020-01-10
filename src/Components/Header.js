5bf0e082a38c54f7c6a3fba09d7481a6bdc8b21d
Colin Turing <colin.koga@gmail.com>
2020-01-09 17:08:21 -0700

    Finish JSX for Header button and styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='header'>
       <h1 className='header-h1'>Monster Set</h1>
-      <button className=''></button>
+      <button className='equip-box-btn'>Equipment Box</button>
     </header>
   )
 }
