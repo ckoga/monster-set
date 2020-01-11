fabff1e96cb4d7ee89ef949d66ca5dd4c885cbc7
Colin Turing <colin.koga@gmail.com>
2020-01-11 15:40:40 -0700

    Add functionality to the Build component

--- src/Components/ArmorCard/ArmorCard.js
+++ src/Components/ArmorCard/ArmorCard.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './ArmorCard.scss'
+import { connect } from 'react-redux';
+import { addEquip } from '../../actions/index';
 
-const ArmorCard = ({ img, name, type, rank, rarity, defense }) => {
+const ArmorCard = ({ img, name, type, rank, rarity, defense, addEquip }) => {
+  let helper = (img, name, type, rank, rarity, defense) => {
+    let equipment = {
+      img: img,
+      name: name,
+      type: type,
+      rank: rank,
+      rarity: rarity,
+      defense: defense
+    }
+    addEquip(equipment)
+  }
   return (
-    <div className='armor-card'>
+    <div className='armor-card' onClick={() => helper(img, name, type, rank, rarity, defense)}>
       <img className='equip-img' src={img} alt='Piece of armor'></img>
       <h4>{name}</h4>
       <ul>
@@ -14,7 +27,10 @@ const ArmorCard = ({ img, name, type, rank, rarity, defense }) => {
       </ul>
     </div>
   )
-
 }
 
-export default ArmorCard;
+export const mapDispatchToProps = dispatch => ({
+  addEquip: (equipment) => dispatch(addEquip(equipment)),
+})
+
+export default connect(null, mapDispatchToProps)(ArmorCard);
--- src/containers/Build.js
+++ src/containers/Build.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import './Build.scss'
+import { connect } from 'react-redux';
+import WeaponCard from '../Components/WeaponCard/WeaponCard';
+import ArmorCard from '../Components/ArmorCard/ArmorCard';
+const uuidv4 = require('uuid/v4');
 
 
-const Build = () => {
+const Build = ({ equipment }) => {
+  let buildSet = () => {
+    return equipment.map(piece => {
+      if (piece.rank !== undefined) {
+          return <ArmorCard
+            key={uuidv4()}
+            img={piece.img}
+            name={piece.name}
+            type={piece.type}
+            rank={piece.rank}
+            rarity={piece.rarity}
+            defense={piece.defense}
+          />
+      } else if (piece.rank === undefined) {
+        return <WeaponCard
+          key={uuidv4()}
+          img={piece.img}
+          name={piece.name}
+          type={piece.type}
+          rarity={piece.rarity}
+          attack={piece.attack.display}
+        /> 
+      } else {
+        return 
+      }
+    })
+  }
+
   return (
     <aside className='build-container'>
       <h3>Build Stats</h3>
-      <ul className='build-list'>
-        
-      </ul>
+      {buildSet()}
     </aside>
   )
 }
 
-export default Build;
+export const mapStateToProps = state => ({
+  equipment: state.equipment
+});
+
+export default connect(mapStateToProps)(Build);
